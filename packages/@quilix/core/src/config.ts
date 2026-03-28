f233e298fd3fc6117d3008499a1c7947c98ab095
Vincent <vincentzhangz@outlook.com>
2026-03-28 11:25:43 +0700

    fix: fix node packages typecheck

--- packages/@quilix/core/src/config.ts
+++ packages/@quilix/core/src/config.ts
@@ -11,6 +11,7 @@ export interface PluginConfig {
 export interface ModuleFederationConfig {
   name: string;
   remotes?: Record<string, string>;
+  exposes?: Record<string, string>;
   shared?: string[];
 }
 
@@ -21,6 +22,7 @@ export function defineConfig(config: QuilixConfig): QuilixConfig {
 export interface ModuleFederationOptions {
   name: string;
   remotes?: Record<string, string>;
+  exposes?: Record<string, string>;
   shared?: string[];
 }
 
